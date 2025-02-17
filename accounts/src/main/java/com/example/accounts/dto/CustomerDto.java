fffe1b9b9d9a09312025971fa0bd7da8419bf251
Mahshad Mollazamani <mmolazamani@gmail.com>
2025-02-17 12:07:27 +0100

    Added validation annotations in AccountsDto and CustomerDto

--- accounts/src/main/java/com/example/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/example/accounts/dto/CustomerDto.java
@@ -1,11 +1,21 @@
 package com.example.accounts.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
+
+    @NotEmpty(message = "Name Cannot be a null or empty")
+    @Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters")
     private String name;
+
+    @NotEmpty(message = "Email cannot be a null or empty")
+    @Email(message = "Please enter a valid email address")
     private String email;
+
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
+
     private AccountsDto accountsDto;
 }
