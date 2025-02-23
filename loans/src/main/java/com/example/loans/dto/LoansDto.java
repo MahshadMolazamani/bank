614b0eb413aa13b2d909669e2817c131db403e2e
Mahshad Mollazamani <mmolazamani@gmail.com>
2025-02-23 14:34:33 +0100

    Add Swagger annotations to ErrorResponseDto, LoansDto and ResponseDto for API documentation

--- loans/src/main/java/com/example/loans/dto/LoansDto.java
+++ loans/src/main/java/com/example/loans/dto/LoansDto.java
@@ -1,5 +1,6 @@
 package com.example.loans.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
@@ -7,24 +8,45 @@ import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
+@Schema(name = "Loans",
+        description = "Schema to hold Loan information"
+)
 public class LoansDto {
     @NotEmpty(message = "MobileNumber cannot be a null or Empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digits")
+    @Schema(
+            description = "Mobile Number of Customer", example = "4365327698"
+    )
     private String mobileNumber;
 
     @NotEmpty(message = "LoanNumber cannot be a null or Empty")
     @Pattern(regexp = "(^$|[0-9]{12})", message = "Loan Number must be 12 digits")
+    @Schema(
+            description = "Loan Number of the customer", example = "548732457654"
+    )
     private String loanNumber;
 
     @NotEmpty(message = "LoanType cannot be a null or Empty")
+    @Schema(
+            description = "Type of the loan", example = "Home Loan"
+    )
     private String loanType;
 
     @Positive(message = "TotalLoan must be a positive number")
+    @Schema(
+            description = "Total loan amount", example = "100000"
+    )
     private int totalLoan;
 
     @PositiveOrZero(message = "AmountPaid must be a positive number ir equal to zero")
+    @Schema(
+            description = "Total loan amount paid", example = "1000"
+    )
     private int amountPaid;
 
     @PositiveOrZero(message = "Total outstanding amount should be equal or greater than zero")
+    @Schema(
+            description = "Total outstanding amount against a loan", example = "99000"
+    )
     private int outstandingAmount;
 }
